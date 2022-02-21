3df6781e5136ae1a1cb60c844716bc12dd2979ef
AnnieChase <dianitaantoniogomez@outlook.es>
2022-02-21 17:02:58 -0600

    Se terminan las katas

--- src/kata3Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-# Agrega el código para crear una variable para un asteroide que viaja a 19 km/s
-# Escribe varias expresiones de prueba para determinar si puedes ver el rayo de luz desde la tierra
-# Agrega las instrucciones que se ejecutarán si las expresiones de prueba son True o False
-velocidadAsteroide = 19
-if (velocidadAsteroide > 20) : print("Se puede observar como cae un asteroide")
-elif(velocidadAsteroide == 20): print("Se puede observar como cae un asteroide")
-else: print("Nada por aquí")
